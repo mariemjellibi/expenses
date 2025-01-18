3f1740a6b38894093cecf51899673969a3d90355
mariemjellibi <mariemjellibi4@gmail.com>
2025-01-18 21:29:42 +0100

    modif

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 //used to connect the backend with the frontend in q seperate file for reusabiliy
 import axios from 'axios'
-const API_URL = 'https://expenses-tracking-pnmh.onrender.com'
+const API_URL = 'https://expenses-server-1.onrender.com'
 export const fetchTransaction =()=> axios.get(`${API_URL}/transactions`);
 export const createTransaction =(transactiondata)=>axios.post(`${API_URL}/transactions`,transactiondata);
 export const deleteTransaction = (id)=>axios.delete(`${API_URL}/transactions/${id}`)
